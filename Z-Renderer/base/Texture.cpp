c4c3e4f2790d9747aec84498b6b2b45a58fcfbec
SATAN_Z <249002031@qq.com>
2018-05-15 13:53:26 +0800

    添加纹理
    Signed-off-by: SATAN_Z <249002031@qq.com>

---
+++ Z-Renderer/base/Texture.cpp
@@ -0,0 +1,9 @@
+//
+//  Texture.cpp
+//  Z-Renderer
+//
+//  Created by SATAN_Z on 2018/5/15.
+//  Copyright © 2018年 SATAN_Z. All rights reserved.
+//
+
+#include "Texture.hpp"
---
+++ Z-Renderer/base/Texture.hpp
@@ -0,0 +1,14 @@
+//
+//  Texture.hpp
+//  Z-Renderer
+//
+//  Created by SATAN_Z on 2018/5/15.
+//  Copyright © 2018年 SATAN_Z. All rights reserved.
+//
+
+#ifndef Texture_hpp
+#define Texture_hpp
+
+#include <stdio.h>
+
+#endif /* Texture_hpp */
